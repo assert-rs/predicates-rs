118b8c48f60c1b484f2c0f009473522e5357ff4b
Vincent de Phily <vincent.dephily@mobile-devices.fr>
2021-05-09 21:02:52 +0100

    chore: update copyright year

--- src/str/dissimilar.rs
+++ src/str/dissimilar.rs
@@ -1,4 +1,4 @@
-// Copyright (c) 2018 The predicates-rs Project Developers.
+// Copyright (c) 2021 The predicates-rs Project Developers.
 //
 // Licensed under the Apache License, Version 2.0 <LICENSE-APACHE or
 // http://www.apache.org/licenses/LICENSE-2.0> or the MIT license
