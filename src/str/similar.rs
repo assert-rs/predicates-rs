8cc3b11947fae749e38a26b670d88aecda3d625b
Vincent de Phily <vincent.dephily@mobile-devices.fr>
2021-05-09 20:54:08 +0100

    feat: Add ratio() and algorithm() methods to SimilarPredicate
    Algorithm is pretty straightforward, but adding ratio() while keeping distance() involved some
    refactorings and API experiments.

--- src/str/similar.rs
+++ src/str/similar.rs
@@ -13,33 +13,50 @@ use crate::reflection;
 use crate::Predicate;
 
 #[derive(Clone, Copy, Debug, PartialEq, Eq)]
-enum DistanceOp {
+enum SimilarOp {
     Similar,
     Different,
 }
 
-impl DistanceOp {
-    fn eval(self, limit: i32, distance: i32) -> bool {
+impl SimilarOp {
+    fn eval(self, similar: bool) -> bool {
         match self {
-            DistanceOp::Similar => distance <= limit,
-            DistanceOp::Different => limit < distance,
+            SimilarOp::Similar => similar,
+            SimilarOp::Different => !similar,
+        }
+    }
+}
+
+#[derive(Clone, Copy, Debug, PartialEq)]
+enum SimilarLimit {
+    Ratio(f32),
+    Changes(u32),
+    // TODO: DiffOps(u32,u32,u32) variant that counts insert/delete/replace separately
+    //       It would also be nice to have the option to not generate DiffOp::Replace
+}
+
+impl fmt::Display for SimilarLimit {
+    fn fmt(&self, f: &mut fmt::Formatter<'_>) -> fmt::Result {
+        match self {
+            SimilarLimit::Ratio(r) => write!(f, "ratio({})", r),
+            SimilarLimit::Changes(d) => write!(f, "changes({})", d),
         }
     }
 }
 
 /// Predicate that diffs two strings.
 ///
-/// This is created by the `predicate::str::similar`.
-#[derive(Debug, Clone, PartialEq, Eq)]
+/// This is created by [`similar3()`](crate::str::similar3) or [`diff3()`](crate::str::diff3).
+#[derive(Debug, Clone, PartialEq)]
 pub struct SimilarPredicate {
     old: borrow::Cow<'static, str>,
     algorithm: similar::Algorithm,
-    distance: i32,
-    op: DistanceOp,
+    op: SimilarOp,
+    limit: SimilarLimit,
 }
 
 impl SimilarPredicate {
-    /// The maximum allowed edit distance.
+    /// The maximum allowed number of changes.
     ///
     /// Default: `0`
     ///
@@ -47,25 +64,102 @@ impl SimilarPredicate {
     ///
     /// ```
     /// use predicates::prelude::*;
+    /// use predicates::str::similar3;
     ///
-    /// let predicate_fn = predicate::str::similar3("Hello World!").distance(1);
-    /// assert_eq!(true, predicate_fn.eval("Hello World!"));
-    /// assert_eq!(true, predicate_fn.eval("Hello World"));
+    /// let s1 = "Hello World!";
+    /// let s2 = "Hello... World?";
     ///
-    /// assert_eq!(false, predicate_fn.eval("Hello... World?"));
-    /// let dist = predicate_fn.find_case(false, "Hello... World?").unwrap().product_value("distance").unwrap();
-    /// assert_eq!("2", &dist);
+    /// assert_eq!(false, similar3(s1).eval(s2));
+    /// assert_eq!(true, similar3(s1).changes(2).eval(s2));
+    /// assert_eq!("changes(2)", &similar3(s1).changes(2).find_case(true, s2).unwrap().product_value("measure").unwrap());
     /// ```
-    pub fn distance(mut self, distance: i32) -> Self {
-        self.distance = distance;
+    pub fn changes(mut self, changes: u32) -> Self {
+        self.limit = SimilarLimit::Changes(changes);
         self
     }
 
-    fn new(old: borrow::Cow<'static, str>, op: DistanceOp) -> Self {
-        Self{old, algorithm: similar::Algorithm::Myers, distance: 0, op}
+    /// The minimum required [similarity](similar::get_diff_ratio).
+    ///
+    /// Default: `1.0`
+    ///
+    /// # Examples
+    ///
+    /// ```
+    /// use predicates::prelude::*;
+    /// use predicates::str::similar3;
+    ///
+    /// let s1 = "Hello World!";
+    /// let s2 = "Hello... World?";
+    ///
+    /// assert_eq!(false, similar3(s1).eval(s2));
+    /// assert_eq!(true, similar3(s1).ratio(0.8).eval(s2));
+    /// assert_eq!("ratio(0.8148148)", &similar3(s1).find_case(false, s2).unwrap().product_value("measure").unwrap());
+    /// ```
+    pub fn ratio(mut self, ratio: f32) -> Self {
+        // FIXME: clamp to 0..1 ? Panic ?
+        self.limit = SimilarLimit::Ratio(ratio);
+        self
     }
 
-    fn diff(&self, chunks: &Vec<similar::DiffOp>) -> String {
+    /// The [diffing algorithm](similar::Algorithm).
+    ///
+    /// Default: [`Myers`](similar::Algorithm::Myers)
+    ///
+    /// # Examples
+    ///
+    /// ```
+    /// use predicates::prelude::*;
+    /// use predicates::str::similar3;
+    /// use similar::Algorithm::*;
+    ///
+    /// let s1 = "Hello World!";
+    /// let s2 = "Hello... World?";
+    /// for (pred, expect) in vec![(similar3(s1), "ratio(0.8148148)"),
+    ///                            (similar3(s1).algorithm(Myers), "ratio(0.8148148)"),
+    ///                            (similar3(s1).algorithm(Lcs), "ratio(0.44444445)"),
+    ///                            (similar3(s1).algorithm(Patience), "ratio(0.8148148)")] {
+    ///   let m = pred.find_case(false, s2).unwrap().product_value("measure").unwrap();
+    ///   assert_eq!(expect, &m);
+    /// }
+    /// ```
+    pub fn algorithm(mut self, alg: similar::Algorithm) -> Self {
+        self.algorithm = alg;
+        self
+    }
+
+    fn new(old: borrow::Cow<'static, str>, op: SimilarOp) -> Self {
+        Self {
+            old,
+            algorithm: similar::Algorithm::Myers,
+            op,
+            limit: SimilarLimit::Ratio(1.0),
+        }
+    }
+
+    fn run(&self, new: &str) -> (bool, Vec<similar::DiffOp>, SimilarLimit) {
+        use similar::DiffOp::*;
+        use SimilarLimit::*;
+
+        let chunks =
+            similar::capture_diff_slices(self.algorithm, self.old.as_bytes(), new.as_bytes());
+        match self.limit {
+            Ratio(r) => {
+                let ratio = similar::get_diff_ratio(&chunks, self.old.len(), new.len());
+                (self.op.eval(ratio >= r), chunks, Ratio(ratio))
+            }
+            Changes(d) => {
+                let changes = chunks.iter().fold(0, |n, c| match c {
+                    Equal { .. } => n,
+                    _ => n + 1,
+                });
+                (self.op.eval(changes <= d), chunks, Changes(changes))
+            }
+        }
+    }
+
+    /// Output a formated diff
+    fn diff(&self, old: &str, new: &str, chunks: &[similar::DiffOp]) -> String {
+        use similar::DiffOp::*;
         use std::fmt::Write;
 
         let mut f = String::with_capacity(chunks.len());
@@ -88,30 +182,17 @@ impl SimilarPredicate {
     }
 }
 
-// TODO `similar offers a diff_ratio which might be better
-// TODO s/distance/changes/ ? Reserve "distance" for some levenstein-like metric ?
-fn distance(chunks: &Vec<similar::DiffOp>) -> i32 {
-    use similar::DiffOp::*;
-    chunks.iter().fold(0, |n, c| match c {
-        Equal{..} => n,
-        _ => n + 1,
-    })
-}
-
 impl Predicate<str> for SimilarPredicate {
     fn eval(&self, new: &str) -> bool {
-        let chunks = similar::capture_diff_slices(self.algorithm, self.old.as_bytes(), new.as_bytes());
-        self.op.eval(self.distance, distance(&chunks))
+        self.run(new).0
     }
 
     fn find_case<'a>(&'a self, expected: bool, new: &str) -> Option<reflection::Case<'a>> {
-        let chunks = similar::capture_diff_slices(self.algorithm, self.old.as_bytes(), new.as_bytes());
-        let distance = distance(&chunks);
-        let result = self.op.eval(self.distance, distance);
+        let (result, chunks, measure) = self.run(new);
         if result == expected {
             Some(
                 reflection::Case::new(Some(self), result)
-                    .add_product(reflection::Product::new("distance", distance))
+                    .add_product(reflection::Product::new("measure", measure))
                     .add_product(reflection::Product::new(
                         "diff",
                         self.diff(&self.old, new, &chunks),
@@ -133,8 +214,8 @@ impl reflection::PredicateReflection for SimilarPredicate {
 impl fmt::Display for SimilarPredicate {
     fn fmt(&self, f: &mut fmt::Formatter<'_>) -> fmt::Result {
         match self.op {
-            DistanceOp::Similar => write!(f, "var - original <= {}", self.distance),
-            DistanceOp::Different => write!(f, "{} < var - original", self.distance),
+            SimilarOp::Similar => write!(f, "original is similar to var by {}", self.limit),
+            SimilarOp::Different => write!(f, "original differs from var by {}", self.limit)
         }
     }
 }
@@ -158,7 +239,7 @@ pub fn diff3<S>(old: S) -> SimilarPredicate
 where
     S: Into<borrow::Cow<'static, str>>,
 {
-    SimilarPredicate::new(old.into(), DistanceOp::Different)
+    SimilarPredicate::new(old.into(), SimilarOp::Different)
 }
 
 /// Creates a new `Predicate` that checks strings for how similar they are.
@@ -180,5 +261,5 @@ pub fn similar3<S>(old: S) -> SimilarPredicate
 where
     S: Into<borrow::Cow<'static, str>>,
 {
-    SimilarPredicate::new(old.into(), DistanceOp::Similar)
+    SimilarPredicate::new(old.into(), SimilarOp::Similar)
 }
